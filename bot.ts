03be77c148453c378178ca6ecdd2bc0ea80e59e0
Mikiyas <mymikiaa11@gmail.com>
2024-06-23 11:07:36 +0300

    made the bot use updated version of dep

--- bot.ts
+++ bot.ts
@@ -1,7 +1,39 @@
-import { Bot } from "./deps.deno.ts";
+import { Bot, 
+    Context, session,
+    type Conversation,
+    type ConversationFlavor,
+    conversations,
+    createConversation,
+ } from "./deps.deno.ts";
 
-export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
+ type MyContext = Context & ConversationFlavor;
+ type MyConversation = Conversation<MyContext>;
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+export const bot = new Bot<MyContext>(Deno.env.get("BOT_TOKEN") || "");
+
+bot.use(session({ initial: () => ({}) }))
+bot.use(conversations());
+
+async function movie(conversation: MyConversation, ctx: MyContext) {
+    await ctx.reply("How many favorite movies do you have?");
+    const count = await conversation.form.number();
+    const movies: string[] = [];
+    for (let i = 0; i < count; i++) {
+      await ctx.reply(`Tell me number ${i + 1}!`);
+      const titleCtx = await conversation.waitFor(":text");
+      movies.push(titleCtx.msg.text);
+    }
+    await ctx.reply("Here is a better ranking!");
+    movies.sort();
+    await ctx.reply(movies.map((m, i) => `${i + 1}. ${m}`).join("\n"));
+  }
+
+bot.use(createConversation(movie));
+
+bot.command("start", async(ctx) => {
+    await   ctx.conversation.enter("movie");
+    // ctx.reply("Welcome! Up and running.")
+
+});
 
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
--- deps.deno.ts
+++ deps.deno.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/grammy@v1.8.3/mod.ts";
+export * from "https://deno.land/x/grammy@v1.25.1/mod.ts";
 export * from "https://deno.land/x/grammy_conversations@v1.2.0/mod.ts"
