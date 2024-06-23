7c3c18b0118a3fbc55e36b42ccd797cc71ccc250
Mikiyas <mymikiaa11@gmail.com>
2024-06-23 15:07:09 +0300

    send user token and create token

--- bot.ts
+++ bot.ts
@@ -4,7 +4,13 @@ import { Bot,
     type ConversationFlavor,
     conversations,
     createConversation,
+    createClient,
  } from "./deps.deno.ts";
+
+ const supabase = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_ANON_KEY") ?? ""
+);
 
  type MyContext = Context & ConversationFlavor;
  type MyConversation = Conversation<MyContext>;
@@ -15,7 +21,7 @@ bot.use(session({ initial: () => ({}) }))
 bot.use(conversations());
 
 async function movie(conversation: MyConversation, ctx: MyContext) {
-    await ctx.reply("How many favorite movies do you have?");
+    await ctx.reply("How are you doing?");
     const count = await conversation.form.number();
     const movies: string[] = [];
     for (let i = 0; i < count; i++) {
@@ -31,9 +37,34 @@ async function movie(conversation: MyConversation, ctx: MyContext) {
 bot.use(createConversation(movie));
 
 bot.command("start", async(ctx) => {
-    await   ctx.conversation.enter("movie");
-    // ctx.reply("Welcome! Up and running.")
-
+    // await   ctx.conversation.enter("movie");
+    ctx.reply("Welcome! use /generate to get your token and connect your account")
 });
+
+bot.command("generate", async(ctx) => {
+ const id = ctx.from?.id;
+  // generate a 23 character token
+  const token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  // add database secret as a check for future  
+  const {data, error} = await supabase.from("telegra_profiles").insert({
+      telegram_id: id,
+      token: token,
+      tg_username: ctx.from?.username,
+      first_name: ctx.from?.first_name,
+      last_name: ctx.from?.last_name,
+      is_public: false,
+    });
+    if (error) {
+       console.log(error);
+       ctx.reply("An error occured, please try again later");
+    } else {
+      // reply with mark down sytaxse
+      console.log(data)
+      ctx.reply(
+        `Here is your token: \`${token}\`\n\nPlease keep it safe, you will need it to connect your account`
+      )
+    }
+})
+
 
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
--- deps.deno.ts
+++ deps.deno.ts
@@ -1,2 +1,3 @@
 export * from "https://deno.land/x/grammy@v1.25.1/mod.ts";
 export * from "https://deno.land/x/grammy_conversations@v1.2.0/mod.ts"
+export  {createClient} from "https://esm.sh/@supabase/supabase-js@2.8.0"
