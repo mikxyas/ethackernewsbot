ca1c2689c087a9fca84558b7fc2bfdbfc85b370b
Mikiyas <mymikiaa11@gmail.com>
2024-06-25 10:29:05 +0300

    bot detects channel status

--- bot.ts
+++ bot.ts
@@ -38,33 +38,58 @@ bot.use(createConversation(movie));
 
 bot.command("start", (ctx) => {
     // await   ctx.conversation.enter("movie");
-    ctx.reply("Welcome! use /generate to get your token and connect your account to Hacker News et")
+    ctx.reply("Hacker News Bot here 🗿\n open web app to explore")
 });
 
-bot.command("generate", async(ctx) => {
- const id = ctx.from?.id;
-  // generate a 23 character token
-  const big_token = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
-  const token = big_token.substring(0, 21);
-  // add database secret as a check for future  
-  const {data, error} = await supabase.from("telegram_profiles").insert({
-      tg_id: id,
-      verf_token: token,
-      tg_username: ctx.from?.username,
-      first_name: ctx.from?.first_name,
-      last_name: ctx.from?.last_name,
-      is_public: false,
-    });
-    if (error) {
-       console.log(error);
-       ctx.reply("It seems you are already connected");
-    } else {
-      // reply with mark down 
-      console.log(data)
-      ctx.reply(`Here is your token: \`${token}\`\n\n Go to https://show.eacc.et/telegram and paste the token there`
-      )
-    }
-})
+// Handle my_chat_member updates
+bot.on('my_chat_member', async (ctx) => {
+  const chat = ctx.chat;
+  const newStatus = ctx.myChatMember.new_chat_member.status;
+  const oldStatus = ctx.myChatMember.old_chat_member.status;
+
+  if (newStatus === 'administrator' && oldStatus !== 'administrator') {
+    // Bot has been added to a channel and promoted to administrator
+    await ctx.api.sendMessage(chat.id, 'Hello! I have been added to your channel and can now post messages.');
+  } else if (newStatus === 'kicked') {
+    // Bot has been removed from the channel
+    ctx.api.sendMessage(chat.id, 'Goodbye! I have been removed from your channel.');
+    console.log(`Bot was removed from channel ${chat.id}`);
+  }
+});
+
+// bot.command("generate", async(ctx) => {
+//  const id = ctx.from?.id;
+//   // generate a 23 character token
+//   const big_token = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
+//   const token = big_token.substring(0, 21);
+//   // add database secret as a check for future  
+//   const {data, error} = await supabase.from("telegram_profiles").insert({
+//       tg_id: id,
+//       verf_token: token,
+//       tg_username: ctx.from?.username,
+//       first_name: ctx.from?.first_name,
+//       last_name: ctx.from?.last_name,
+//       is_public: false,
+//     });
+//     if (error) {
+//       // give detailed error messages if the user already exists say so if there is an error also say so
+//       if(error.code === "23505"){
+//         console.log(error);
+//         ctx.reply("It seems you already generated a token");
+//       }else if(error.code === "23514"){
+//         console.log(error);
+//         ctx.reply("There was an error generating your token");
+//       }else {
+//         console.log(error);
+//         ctx.reply("There was an error generating your token");
+//       }
+//     } else {
+//       // reply with mark down 
+//       console.log(data)
+//       ctx.reply(`Here is your token: \`${token}\`\n\n Go to https://show.eacc.et/telegram and paste the token there`
+//       )
+//     }
+// })
 
 
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
